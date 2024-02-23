c6080eedb5764a8c3207f0783322ab6d1d7ee4e8
Stranger667 <88589797+StrangerZ667@users.noreply.github.com>
2024-02-23 14:12:16 +0700

    average.cpp Updated

--- average.cpp
+++ average.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
+int main(int argc,char *argv[]){
+    double sum=0;
+    int i=0;
+    if(argc < 2) cout << "Please input numbers to find average.\n";
+    else {
+        while(i<argc){
+            sum+=atoi(argv[i]);
+            i++;
+        }
+        cout << "---------------------------------\n";
+        cout << "Average of " << i-1 << " numbers = " << sum/(i-1) << endl;
+        cout << "---------------------------------";
+    }
+}
